8228e155bea94181b6862e87726757f2617a76bc
Dina Hamdan <dina.m.hamdane@gmail.com>
2024-06-06 13:32:49 +0200

    started javascript and added timeout redirection.

--- js/buy.js
+++ js/buy.js
@@ -1,7 +1,21 @@
 
 let buyLink = document.getElementById("buy-link");
+let eTickets = document.getElementById("e-tickets");
+let buyPage = document.getElementById("buy-tickets");
 
-buyLink.addEventListener('click',()=>
-{
-    
-})
+buyLink.addEventListener('click', () => {
+    eTickets.style.display = "none";
+    console.log('clicked on buy link')
+    buyPage.style.display = "block";
+
+});
+
+let payLink = document.getElementById("select-buy");
+let selectTicketContainer = document.getElementById("child-one");
+let termsCondtionsContainer = document.getElementById("terms-conditions");
+
+payLink.addEventListener('click', () => {
+    selectTicketContainer.style.display = "none";
+    termsCondtionsContainer.style.display = "block";
+
+});
