727d48d4f314131defba12e75a77b6fd41c518bb
Dina Hamdan <dina.m.hamdane@gmail.com>
2024-06-07 12:12:02 +0200

    added card and cvv info and close options

--- js/pay.js
+++ js/pay.js
@@ -8,3 +8,30 @@ submitButton.addEventListener('click', () => {
     shopIcon.style.display = "inline-block";
 });
 
+
+let firstQuestionMark = document.getElementById("first-question-mark");
+let hiddenCardInfo = document.getElementById("hidden-card-info");
+firstQuestionMark.addEventListener('click', () => {
+    hiddenCardInfo.style.display = "block";
+})
+let closeCardInfo = document.getElementById("close-card");
+closeCardInfo.addEventListener('click', () => {
+    hiddenCardInfo.style.display = "none";
+})
+
+
+let secondQuestionMark = document.getElementById("second-question-mark");
+let hiddenCvvInfo = document.getElementById("hidden-cvv-info");
+secondQuestionMark.addEventListener('click', () => {
+    hiddenCvvInfo.style.display = "block";
+})
+
+let closeCvvInfo = document.getElementById("close-cvv");
+closeCvvInfo.addEventListener('click', () => {
+    hiddenCvvInfo.style.display = "none";
+})
+// let languageBtn = document.getElementById("language-button");
+// languageBtn.addEventListener('click', () => {
+//     languageBtn.innerHTML = 'ESP';
+//     languageBtn.innerHTML = 'ENG';
+// })
