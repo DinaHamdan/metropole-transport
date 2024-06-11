6270dfc9886e4c77523b38539e84f6e1001cf4d9
Dina Hamdan <dina.m.hamdane@gmail.com>
2024-06-11 13:41:02 +0200

    fixed sw and manifest

--- sw.js
+++ sw.js
@@ -15,9 +15,36 @@ var cacheName = 'hello-pwa';
 var filesToCache = [
     '/',
     '/index.html',
-    '/asset/img',
+    '/asset/img/logo1.avif',
+    '/asset/img/logo2.avif',
+    '/asset/img/logo3.avif',
+    '/asset/img/logo3bis.avif',
+    '/asset/img/logo4.avif',
+    '/asset/img/logo5.avif',
+    '/asset/img/logo6.avif',
+    '/asset/img/logo7.avif',
+    '/asset/img/logo8.avif',
+    '/asset/img/logo9.avif',
+    '/asset/img/logo9bis.avif',
+    '/asset/img/logo10.avif',
+    '/asset/img/logo11.avif',
+    '/asset/img/logo12.avif',
+    '/asset/img/logo13.avif',
+    '/asset/img/logo14.avif',
+    '/asset/img/logo15.avif',
+    '/asset/img/logo16.avif',
+    '/asset/img/logo17.avif',
+    '/asset/img/more.avif',
+    '/asset/img/card.avif',
+    '/asset/img/correct.avif',
+    '/asset/img/cross.avif',
+    '/asset/img/down-arrow.avif',
+    '/asset/img/hour-glass.avif',
+    '/asset/img/icon.avif',
+    '/asset/img/left-chevron.avif',
     '/styles/styles.css',
     '/js/main.js'];
+
 self.addEventListener('install', function (e) {
     e.waitUntil(
         caches.open(cacheName).then(function (cache) {
